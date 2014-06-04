42c2cab302c2b33c03a6ae220ec631da73d84d66
leandrovillalobos <l.e.v.s@hotmail.com>
2014-06-04 12:44:11 -0430

    Create ciclos.php
    aqui se realiza la accion definida en el formulario //Leandro Villalobos

---
+++ ciclos.php
@@ -0,0 +1,17 @@
+<?php
+	$aux = 1;
+
+	echo "Bucle while<br><br><br>";
+
+	while ($aux <= $_GET['num1']) {
+		echo "Los bucles son faciles<br>";
+		$aux++;
+	}
+	
+
+	echo "<br>Fin del ciclo";
+
+
+
+
+?>
