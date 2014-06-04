ad0e754dc7b456b925ffe4cd62b8ccce9d6810dc
leandrovillalobos <l.e.v.s@hotmail.com>
2014-06-04 12:41:47 -0430

    Create bucles.php
    formulario del bucle //Leandro Villalobos

---
+++ bucles.php
@@ -0,0 +1,20 @@
+<head>
+	<title>Formulario Bucles</title>
+</head>
+<body>
+
+	<form method = "GET" action = "ciclos.php">
+		Cantidad de veces que se repetira el ciclo
+		<input type = "int" name = "num1" autofocus><br>
+		
+		<input type = "submit" value = "Enviar...">
+
+
+
+
+	</form>
+
+	
+
+</body>
+</html>
